cac1a290d36dff55967d4c568ada397a97675dd2
Vladimir Tolstov <tolstov@gmail.com>
2025-04-23 15:37:51 +0700

    added example connection to db

---
+++ src/main/java/DatabaseConnectionManager.java
@@ -0,0 +1,23 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnectionManager {
+
+    private static final HikariDataSource HIKARI_DATA_SOURCE;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite:src/main/resources/database");
+        config.setDriverClassName("org.sqlite.JDBC");
+
+        HIKARI_DATA_SOURCE = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return HIKARI_DATA_SOURCE.getConnection();
+    }
+
+}
---
+++ src/main/java/Example.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        final String query = "SELECT * FROM Currencies WHERE ID = ?";
+
+        try (Connection connection = DatabaseConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, 1);
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                System.out.println(resultSet.getString("ID"));
+                System.out.println(resultSet.getString("Code"));
+                System.out.println(resultSet.getString("FullName"));
+                System.out.println(resultSet.getString("Sign"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
