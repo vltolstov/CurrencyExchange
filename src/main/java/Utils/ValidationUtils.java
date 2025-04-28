84b7f6459f522ee0594715eee2233a83a742537a
Vladimir Tolstov <tolstov@gmail.com>
2025-04-28 17:03:35 +0700

    added currency code validation

---
+++ src/main/java/Utils/ValidationUtils.java
@@ -0,0 +1,37 @@
+package Utils;
+
+import exception.InvalidParameterException;
+
+import java.util.Currency;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ValidationUtils {
+
+    private static Set<String> currencyCodes;
+
+    public static void validate (){
+        //валидация ввода при добавлении новой валюты
+    }
+
+    public static void validateCurrencyCode(String code){
+
+        if(code.length() != 3){
+            throw new InvalidParameterException("Invalid currency code length");
+        }
+
+        if(currencyCodes == null){
+            Set<Currency> currencies = Currency.getAvailableCurrencies();
+
+            currencyCodes = currencies.stream()
+                    .map(Currency::getCurrencyCode)
+                    .collect(Collectors.toSet());
+        }
+
+        if(!currencyCodes.contains(code)){
+            throw new InvalidParameterException("Currency code not valid. Use ISO 4217 format");
+        }
+
+    }
+
+}
