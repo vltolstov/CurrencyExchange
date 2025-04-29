b53e41732420ddbe722bd87deff2b7a5c1b6b610
Vladimir Tolstov <tolstov@gmail.com>
2025-04-29 15:24:36 +0700

    added error response dto

---
+++ src/main/java/dto/ErrorResponseDto.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDto {
+
+    private Integer code;
+
+    private String message;
+}
