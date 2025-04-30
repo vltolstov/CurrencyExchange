975e3b33d9434d88b1ab56dabfe2b48c589bbae1
Vladimir Tolstov <tolstov@gmail.com>
2025-04-30 15:42:30 +0700

    changed database path

--- src/main/java/Utils/DatabaseConnectionManager.java
+++ src/main/java/Utils/DatabaseConnectionManager.java
@@ -12,7 +12,7 @@ public class DatabaseConnectionManager {
 
     static {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:sqlite:src/main/resources/database");
+        config.setJdbcUrl("jdbc:sqlite:database");
         config.setDriverClassName("org.sqlite.JDBC");
 
         HIKARI_DATA_SOURCE = new HikariDataSource(config);
