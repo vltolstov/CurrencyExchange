c300760fbf7cc7b58a66651352838700cc4e9d61
Vladimir Tolstov <tolstov@gmail.com>
2025-05-06 16:53:29 +0700

    completed findByCodes method

--- src/main/java/dao/JdbcExchangeRateDao.java
+++ src/main/java/dao/JdbcExchangeRateDao.java
@@ -18,23 +18,27 @@ public class JdbcExchangeRateDao implements ExchangeRateDao {
     @Override
     public Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode) {
 
-        final String query = "SELECT" +
-                "    er.id AS id," +
-                "    bc.id AS base_id," +
-                "    bc.Code AS base_code," +
-                "    bc.FullName AS base_name," +
-                "    bc.Sign AS base_sign," +
-                "    tc.id AS target_id," +
-                "    tc.Code AS target_code," +
-                "    tc.FullName AS target_name," +
-                "    tc.Sign AS target_sign," +
-                "    er.rate AS rate" +
-                "FROM Exchange_rates er" +
-                "    JOIN Currencies bc ON er.base_currency_id = bc.id" +
-                "    JOIN Currencies tc ON er.target_currency_id = tc.id" +
-                "WHERE (base_currency_id = (SELECT c.id FROM currencies c WHERE c.code = ?)" +
-                "           AND target_currency_id = (SELECT c2.id FROM currencies c2 WHERE c2.code = ?)" +
-                ")";
+        final String query = """
+                        SELECT
+                            er.id AS id,
+                            bc.id AS base_id,
+                            bc.Code AS base_code,
+                            bc.FullName AS base_name,
+                            bc.Sign AS base_sign,
+                            tc.id AS target_id,
+                            tc.Code AS target_code,
+                            tc.FullName AS target_name,
+                            tc.Sign AS target_sign,
+                            er.rate AS rate
+                        FROM Exchange_rates er
+                        JOIN Currencies bc ON er.base_currency_id = bc.id
+                        JOIN Currencies tc ON er.target_currency_id = tc.id
+                        WHERE (
+                            base_currency_id = (SELECT c.id FROM currencies c WHERE c.code = ?)
+                        AND
+                            target_currency_id = (SELECT c2.id FROM currencies c2 WHERE c2.code = ?)
+                        )
+                        """;
 
         try(Connection connection = DatabaseConnectionManager.getConnection();
             PreparedStatement statement = connection.prepareStatement(query)) {
@@ -45,7 +49,7 @@ public class JdbcExchangeRateDao implements ExchangeRateDao {
             ResultSet resultSet = statement.executeQuery();
 
             if(resultSet.next()){
-                //return Optional.of(new ExchangeRate(resultSet))
+                return Optional.of(getExchangeRate(resultSet));
             }
         } catch (SQLException e) {
             throw new DatabaseOperationException("Exchange rate with code" + baseCurrencyCode + " and currency code" + targetCurrencyCode + " not found");
@@ -76,20 +80,20 @@ public class JdbcExchangeRateDao implements ExchangeRateDao {
 
     private static ExchangeRate getExchangeRate(ResultSet resultSet) throws SQLException {
         return new ExchangeRate(
-                resultSet.getInt("id"),
-                new Currency(
-                    resultSet.getInt("base_id"),
-                    resultSet.getString("base_code"),
-                    resultSet.getString("base_name"),
-                    resultSet.getString("base_sign")
-                ),
-                new Currency(
-                    resultSet.getInt("target_id"),
-                    resultSet.getString("target_code"),
-                    resultSet.getString("target_name"),
-                    resultSet.getString("target_sign")
-                ),
-                resultSet.getDouble("rate")
+            resultSet.getInt("id"),
+            new Currency(
+                resultSet.getInt("base_id"),
+                resultSet.getString("base_code"),
+                resultSet.getString("base_name"),
+                resultSet.getString("base_sign")
+            ),
+            new Currency(
+                resultSet.getInt("target_id"),
+                resultSet.getString("target_code"),
+                resultSet.getString("target_name"),
+                resultSet.getString("target_sign")
+            ),
+            resultSet.getDouble("rate")
         );
     }
 }
