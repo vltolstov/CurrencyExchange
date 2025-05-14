3bcf8f75e8b9a67d2822ad6e4a2fdb2b73ab8f1f
Vladimir Tolstov <tolstov@gmail.com>
2025-05-14 15:18:28 +0700

    added noArgConstructor

--- src/main/java/models/Currency.java
+++ src/main/java/models/Currency.java
@@ -2,9 +2,11 @@ package models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Currency {
 
     private Integer id;
